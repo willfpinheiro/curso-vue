a6bc76c9fa825f75b293537b8c43759615cf7fec
willfpinheiro <42159903+willfpinheiro@users.noreply.github.com>
2020-04-16 22:41:29 -0300

    Desafio 3

--- dom/desafio-03/app.js
+++ dom/desafio-03/app.js
@@ -1,6 +1,18 @@
 new Vue({
-    el: '#desafio',
-    data: {
-        valor: 0
-    }
+	el: "#desafio",
+	data: {
+		valor: 0,
+	},
+	watch: {
+		resultado(novo, antigo) {
+			setTimeout(() => {
+				this.valor = 0;
+			}, 5000);
+		},
+	},
+	computed: {
+		resultado() {
+			return this.valor == 37 ? "Valor Igual" : "Valor Diferente";
+		},
+	},
 });
